445037f1681f8d072a3f2514dba32bc5df3f38da
Andrew Krutsch <andr.krutsch@gmail.com>
2024-07-17 20:29:44 +0500

    Django 5 By Example - Chapter 2

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 from django.utils import timezone
 
 # Create your models here.
@@ -13,8 +14,8 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -36,5 +37,33 @@ class Post(models.Model):
             ),
         )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
+    
+    def get_absolute_url(self) -> str:
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = (
+            'created',
+        )
+        indexes = (
+            models.Index(
+                fields=(
+                    'created',
+                )
+            ),
+        )
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,94 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
-from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 # Create your views here.
 def post_list(request: HttpRequest) -> HttpResponse:
-    posts = Post.objects.all()
+    posts_list = Post.objects.all()
 
-    context = {
-        'posts': posts,
-    }
+    paginator = Paginator(object_list=posts_list, per_page=3)
+    
+    page_number = request.GET.get(key='page', default=1)
+
+    try:
+        posts = paginator.page(number=page_number)
+    except EmptyPage:
+        posts = paginator.page(number=paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(number=1)
+
+    context = dict(
+        posts=posts,
+    )
 
     return render(request=request, template_name='blog/post/list.html', context=context)
 
-def post_detail(request: HttpRequest, id: int) -> HttpResponse:
-    post = get_object_or_404(klass=Post, pk=id, status=Post.Status.PUBLISHED)
+
+def post_detail(request: HttpRequest, year: int, month: int, day: int, slug: str) -> HttpResponse:
+    post = get_object_or_404(klass=Post, publish__year=year, publish__month=month, publish__day=day, slug=slug, status=Post.Status.PUBLISHED)
+    comments = post.comments.filter(active=True)
+
+    form = CommentForm()
     
-    context = {
-        'post': post
-    }
+    context = dict(
+        post=post,
+        comments=comments,
+        form=form,
+    )
 
     return render(request=request, template_name='blog/post/detail.html', context=context)
+
+
+def post_share(request: HttpRequest, post_id: int) -> HttpResponse:
+    post = get_object_or_404(klass=Post, pk=post_id, status=Post.Status.PUBLISHED)
+
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(data=request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            
+            subject = '{} {} recommends you read {}'.format(cleaned_data.get('name'), cleaned_data.get('email'), post.title)
+            message = 'Read {} at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cleaned_data.get('name'), cleaned_data.get('comments'))
+
+            send_mail(subject=subject, message=message, from_email=None, recipient_list=[cleaned_data.get('to')])
+            
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    context = dict(
+        post=post,
+        form=form,
+        sent=sent,
+    )
+
+    return render(request=request, template_name='blog/post/share.html', context=context)
+
+
+@require_POST
+def post_comment(request: HttpRequest, post_id: int) -> HttpResponse:
+    post = get_object_or_404(klass=Post, pk=post_id, status=Post.Status.PUBLISHED)
+
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment: Comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    context = dict(
+        post=post,
+        comment=comment,
+        form=form,
+    )
+
+    return render(request=request, template_name='blog/post/comment.html', context=context)
